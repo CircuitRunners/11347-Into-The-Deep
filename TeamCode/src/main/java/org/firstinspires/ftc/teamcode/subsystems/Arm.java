d01c7640191ab2941891b79c06d15e2ad6580d05
PolishSushi <matthew.xia@wheelermagnet.com>
2024-10-08 16:37:29 -0400

    message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.teamcode.subsystems;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class Arm extends SubsystemBase{
+    DcMotorEx = armMotor;
+    pub
+    public Arm(HardwareMap hardwareMap){
+        this(hardwareMap);
+        armMotor = hardwareMap.get(ServoImplEx.class, "armmotor");
+    }
 
-public class Arm {
+    //TeleOp
+    public void Power(double power) {
+        armMotor.setPower(power);
+    }
+
+
+
+    //autonomous stuff
+    public void upPower() {
+        armMotor.setPower(0.5);
+    }
+    public void downPower() {
+        armMotor.setPower(-0.5);
+    }
+
+
+    mainMotor.setPower(0.5); 
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/MainTeleOp.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.teleOp;
+
+public class MainTeleOp extends CommandOpMode {
+    public Arm arm;
+    arm.Power(gamepad2.left_stick_y);
+}
