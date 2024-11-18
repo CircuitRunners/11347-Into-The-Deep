2a5624787c5730f07cd2724d98aa10bf97b48caf
Justin Racel <75274393+ConcaveEgg@users.noreply.github.com>
2024-11-17 22:35:56 -0500

    Update betterTestAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/betterTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/betterTestAuto.java
@@ -30,7 +30,7 @@ public class betterTestAuto extends OpMode {
     private int pathState;
 
     // Define key poses
-    private Pose startPosition = new Pose(10, 111, Math.toRadians(-90));
+    private Pose startPosition = new Pose(10, 111, Math.toRadians(0));
     private Pose pos1 = new Pose(111, 111, Math.toRadians(-90));
     private Pose pos2 = new Pose(111, 30, Math.toRadians(180));
     private Pose pos3 = new Pose(33, 33, Math.toRadians(90));
