59c46c2e9dab5cdac3d05c10a6db4e3fb1cd53a6
Justin Racel <ConcaveEgg@users.noreply.github.com>
2024-11-21 14:45:55 -0500

    Testing
    made ArmCorrected similar to 2.0, changed to original from deadband using version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArmCorrected.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ArmCorrected.java
@@ -1,14 +1,12 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 import org.firstinspires.ftc.teamcode.support.RunAction;
 
 @Config
@@ -17,11 +15,11 @@ public class ArmCorrected {
         REST(5),
         MID(290),
         AUTO(410),
-        BASKET_HIGH(200),
+        BASKET_HIGH(1000),
         SPECIMEN(500),
-        TOPBAR(328),
+        TOPBAR(1000),
         HOVER_SUB(534),
-        GRAB_SUB(605);
+        GRAB_SUB(2000);
 
         public int position;
 
@@ -34,68 +32,79 @@ public class ArmCorrected {
         }
     }
 
-    public RunAction toTopBar, toGrabPos;
-    private Telemetry telemetry;
-
-    private DcMotorEx armMotor;
-    public PIDController ArmPID;
+    private PIDController controller;
+    public static double p = 0.022, i = 0, d = 0.001; // 0.022, 0, 0.001
+    public static double f = 0.15; // 0.15
+//    public static double p = 0.015, i = 0, d = 0.0008;
+//    public static double f = 0.07;
 
     public static int target = 0;
-    public static double p = 0.015, i = 0, d = 0.0008;
-    public static double f = 0.07;
 
-    public ArmCorrected(HardwareMap hardwareMap, Telemetry telemetry) {
-        this.telemetry = telemetry;
-        this.telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+    public DcMotorEx armMotor;
+
+    public RunAction toTopBar, toGrabPos, toRestPos, toBasketPos;
+
+    public ArmCorrected(HardwareMap hardwareMap) {
+        controller = new PIDController(p, i, d);
 
         armMotor = hardwareMap.get(DcMotorEx.class, "armmotor");
-
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        ArmPID = new PIDController(p, i, d);
+        target = getCurrentPosition();
 
         toTopBar = new RunAction(this::toTopBar);
         toGrabPos = new RunAction(this::toGrabPos);
+        toRestPos = new RunAction(this::toRestPos);
+        toBasketPos = new RunAction(this::toBasketPos);
     }
 
-    public void toTopBar() {
-        setTarget(ArmPositions.TOPBAR.position);
-    }
+    public void update() {
+        controller.setPID(p, i, d);
+        double armPos = getCurrentPosition();
+        double pid = controller.calculate(armPos, target);
+        double ff = Math.sin(Math.toRadians(target)) * f;
 
-    public void toGrabPos() {
-        setTarget(ArmPositions.SPECIMEN.position);
-    }
-
-    public void updatePIDF() {
-        ArmPID.setPID(p, i, d);
-
-        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-
-        double pid = ArmPID.calculate(getArmPosition(), target);
-        double ticks_in_degrees = 0.0;
-        double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
         double power = pid + ff;
 
         armMotor.setPower(power);
-
-        telemetry.addData("Arm pos", getArmPosition());
-        telemetry.addData("Arm target", target);
     }
 
-    public void setTarget(int target) {
-        this.target = target;
-    }
-
-    public int getArmPosition() {
+    public int getCurrentPosition() {
         return armMotor.getCurrentPosition();
     }
 
-    public void init() {
-        ArmPID.setPID(p, i, d);
+    public void setArmTarget(int new_target) {
+        target = new_target;
     }
 
-    public void start() {
-        target = 0;
+    public double getArmCurrent() {
+        return armMotor.getCurrent(CurrentUnit.AMPS);
+    }
+
+    public int getArmTarget() {
+        return target;
+    }
+
+    public void manual(int a) {
+        setArmTarget(target + a * 5);
+    }
+
+    public void toTopBar() {
+        setArmTarget(ArmPositions.BASKET_HIGH.position);
+    }
+    public void toGrabPos() {
+        setArmTarget(ArmPositions.GRAB_SUB.position);
+    }
+    public void toRestPos() {
+        setArmTarget(ArmPositions.REST.position);
+    }
+    public void toBasketPos() {
+        setArmTarget(ArmPositions.BASKET_HIGH.position);
+    }
+
+    public void resetArmPosition() {
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 }
